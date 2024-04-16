b5d44486593ba50cad69c5e160c9c469baab354e
Dennis Peter <peterdennis573@gmail.com>
2024-04-16 12:49:53 +0300

    Updates

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about'>
+        <p className='section-headers'>ABOUT</p>
+    </section>
+  )
+}
+
+export default About
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react"
 import "./Home.scss"
 import Header from "../../Components/Header/Header"
+import About from "../../Components/About/About"
 
 const Home = () => {
   return (
     <Fragment>
         <Header/>
+        <About/>
     </Fragment>
   )
 }
