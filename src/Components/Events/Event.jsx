d4df92c1bb5c0295c54ee5b65cc3efc8c1264c6b
Dennis Peter <peterdennis573@gmail.com>
2024-04-16 13:36:11 +0300

    Updates

---
+++ src/Components/Events/Event.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Event = () => {
+  return (
+    <section className="section ministries">
+         <p className="section-headers">MINISTRIES</p>
+    </section>
+  )
+}
+
+export default Event
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import { Fragment } from "react"
 import "./Home.scss"
 import Header from "../../Components/Header/Header"
 import About from "../../Components/About/About"
+import Event from "../../Components/Events/Event"
 
 const Home = () => {
   return (
     <Fragment>
         <Header/>
         <About/>
+        <Event/>
     </Fragment>
   )
 }
