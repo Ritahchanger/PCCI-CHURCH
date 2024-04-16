32887ad601c5d8a736f714cd64dfd96bc9aded65
Dennis Peter <peterdennis573@gmail.com>
2024-04-16 12:41:51 +0300

    Updates

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import "./Header.scss"
+import "./Header.scss";
 
 const Header = () => {
   return (
     <section className="section header">
       <Navbar />
+      <div className="container">
+        <div className="content-header">
+          <h2 className="big-description">
+            Encounter<br></br>God's Grace
+          </h2>
+          <p className="description">
+            Find solace, strength, and inspiration in the heart of our church.
+          </p>
+          <a href="" className="hero-button">
+            <p> Find a service</p>{" "}
+            <p>
+              {" "}
+              <i class="fa-solid fa-arrow-right-long"></i>
+            </p>
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
