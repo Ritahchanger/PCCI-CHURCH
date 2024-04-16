a1551810e4fa8587c4ceb1712b669c902bbc2cb1
Dennis Peter <peterdennis573@gmail.com>
2024-04-16 07:30:31 +0300

    worked on the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,32 +9,32 @@ const Navbar = () => {
         <div className="logo">
           <a href="#">PCCI</a>
         </div>
-
-        <div className="inner-navigation-menu flex">
-          <ul>
-            <li>
-              <a href="#">HOME</a>
-            </li>
-            <li>
-              <a href="#">ABOUT US</a>
-            </li>
-            <li>
-              <a href="#">SERVICES</a>
-            </li>
-            <li>
-              <a href="#">EVENTS</a>
-            </li>
-            <li>
-              <a href="#">MINISTRIES</a>
-            </li>
-            <li>
-              <a href="#">CONTACT US</a>
-            </li>
-
-            <li>
-              <a href="#">DONATE</a>
-            </li>
-          </ul>
+        <div className="centered-menu flex">
+          <div className="navigation-menu">
+            <ul>
+              <li>
+                <a href="#">HOME</a>
+              </li>
+              <li>
+                <a href="#">ABOUT US</a>
+              </li>
+              <li>
+                <a href="#">SERVICES</a>
+              </li>
+              <li>
+                <a href="#">EVENTS</a>
+              </li>
+              <li>
+                <a href="#">MINISTRIES</a>
+              </li>
+              <li>
+                <a href="#">CONTACT US</a>
+              </li>
+              <li>
+                <a href="#">DONATE</a>
+              </li>
+            </ul>
+          </div>
           <button className="universal-btn">JOIN US</button>
         </div>
 
